3c7f25ec009b13c92c58668b685981230da21447
mesutKoc77 <mkoc1367@gmail.com>
2022-10-19 20:11:16 +0200

    guncel

--- src/test/java/ders07_handleDropdownMenu/C03ileIlgili_Odev.java
+++ src/test/java/ders07_handleDropdownMenu/C03ileIlgili_Odev.java
@@ -1,5 +1,18 @@
 package ders07_handleDropdownMenu;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+
 public class C03ileIlgili_Odev {
     /*
   1. http://zero.webappsecurity.com/ Adresine gidin
@@ -17,4 +30,49 @@ public class C03ileIlgili_Odev {
 13. “Foreign currency cash was successfully purchased.” yazisinin ciktigini kontrol edin.
 
      */
+
+    WebDriver driver;
+
+    @Before
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @Test
+    public void test1() throws InterruptedException {
+        driver.get("http://zero.webappsecurity.com/");
+        driver.findElement(By.className("icon-signin")).click();
+        driver.findElement(By.id("user_login")).sendKeys("username");
+        driver.findElement(By.id("user_password")).sendKeys("password");
+        driver.findElement(By.xpath("//input[@value='Sign in']")).click();
+        driver.navigate().back();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//a[text()='More Services']")).click();
+        driver.findElement(By.id("pay_bills_link")).click();
+        driver.findElement(By.xpath("//a[text()='Purchase Foreign Currency']")).click();
+
+        WebElement DDM=driver.findElement(By.id("pc_currency"));
+        Select select=new Select(DDM);
+        select.selectByVisibleText("Eurozone (euro)");
+        select.getFirstSelectedOption().click();
+        driver.findElement(By.id("pc_amount")).sendKeys("3000");
+        WebElement radioUSD=driver.findElement(By.id("pc_inDollars_true"));
+        Assert.assertFalse(radioUSD.isSelected());
+        driver.findElement(By.id("pc_inDollars_false")).click();
+        driver.findElement(By.id("pc_calculate_costs")).click();
+        driver.findElement(By.id("purchase_cash")).click();
+        String expectedText="Foreign currency cash was successfully purchased.";
+        String actualText=driver.findElement(By.id("alert_content")).getText();
+        Assert.assertEquals(expectedText,actualText);
+
+    }
+
+    @After
+    public void teardown(){
+        driver.close();
+    }
+
 }
